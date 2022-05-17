b8e0583db3511e1e2c402df5310c5c611fb16423
Paulo Hudson <105687302+PauloHudson@users.noreply.github.com>
2022-05-16 23:14:18 -0300

    Create Dontknow.py

--- listapar.py
+++ listapar.py
@@ -2,9 +2,7 @@ i = 0
 for a in range(1, 7):
     a = int(input('Digite um número: '))
     if a % 2 == 0:
-        i = i + a
-    else:
-        print('Este não é um número par não havera soma')
-print (f'A soma foi apenas realizada entre os pares = {i}')
+     i = i + a
+print (f'A soma foi realizada apenas entre os números pares, dando um total de {i}')
 
 
